1a329000df2ad330b7c1af7e1e74ffc196fadf2f
jacka <758168660@qq.com>
2021-06-22 15:54:38 +0800

    增强tm计算公式

--- cal_util.py
+++ cal_util.py
@@ -43,6 +43,11 @@ def get_gene(path):
     return data
 
 
+def cal_gc(tem_gene):
+    # 计算一小片段中gc的含量
+    return (tem_gene.count("C") + tem_gene.count("G")) / len(tem_gene)
+
+
 def cal_tm(temp_gene):
     """
     计算一小段基因(temp_gene)的tm
@@ -76,6 +81,61 @@ def cal_tm(temp_gene):
         -8.2) + CGGC * (-10.6) + GCCG * (-9.8) + GGCC * (-8) + 0.1 + 2.3
     S = AATT * (-22.2) + ATTA * (-20.4) + TAAT * (-21.3) + CAGT * (-22.7) + GTCA * (-22.4) + CTGA * (-21) + GACT * (
         -22.2) + CGGC * (-27.2) + GCCG * (-24.4) + GGCC * (-19.9) - 2.8 + 4.1 - 1.4
-    # TODO 钠离子浓度需要重新设置
-    return (H * 1000) / (S + 1.987 * math.log10((10 ** -4) / 4)) - 273.15 + 16.6 * math.log10(1.)
 
+    # TODO 钠离子浓度需要重新设置
+    # 当Na+ 浓度不是1 mol / L 时，就需要对其校正，并且C_t / 4
+    c_Mon = 0.005  # 输入浓度
+    c_Mg = 1.5e-3  #
+    c_K = 5.0e-2  # mol / L
+    c_Tris = 1.0e-2  # mol / L#
+    c_Na = 1e3  #
+    c_t = 2e-3  # mmol / L#
+
+    kelvins = 273.15
+
+    a = 3.92e-5
+    b = -9.11e-6
+    c = 6.26e-5
+    d = 1.42e-5
+    e = -4.82e-4
+    f = 5.25e-4
+    g = 8.31e-5
+
+    f_GC = cal_gc(temp_gene)
+    n_bp = len(temp_gene)
+
+    tm = (H * 1000) / (S + 1.987 * math.log((c_t /1000) / 4)) + 16.6 * math.log(1.02)
+    # print(tm-kelvins)
+    # print(76.3+kelvins)
+    # tm = 76.3+kelvins
+
+    if c_Mon == 0:
+        tem = 1 / tm + a + b * math.log(c_Mg) + f_GC * (c + d * math.log(c_Mg)) + (
+                e + f * math.log(c_Mg) + g * (math.log(c_Mg) ** 2)) / (2 * (n_bp - 1))
+        return 1 / tem - kelvins
+    else:
+        R = math.sqrt(c_Mg) / c_Mon
+
+        if R < 0.22:
+            c_Mon = c_Na + c_K + c_Tris
+            tem = 1 / tm + (4.29 * f_GC - 3.95) *10e-5 * math.log(c_Mon) + 9.4e-6*(math.log(c_Mon))**2
+            return 1 / tem - kelvins
+        elif R < 6.0:
+
+            a = 3.92e-5*(0.843-0.352*math.sqrt(c_Mon) * math.log(c_Mon))
+            d = 1.42e-5*(1.279-4.03e-3*math.log(c_Mon)-8.03e-3*(math.log(c_Mon))**2)
+            g = 8.31e-5*(0.486-0.258*math.log(c_Mon)+5.25e-3*(math.log(c_Mon))**3)
+
+            tem = 1 / tm + a + b * math.log(c_Mg) + f_GC * (c + d * math.log(c_Mg)) + (
+                    e + f * math.log(c_Mg) + g * (math.log(c_Mg) ** 2)) / (2 * (n_bp - 1))
+            return 1 / tem - kelvins
+        else:
+
+            tem = 1 / tm + a + b * math.log(c_Mg) + f_GC * (c + d * math.log(c_Mg)) + (
+                    e + f * math.log(c_Mg) + g * (math.log(c_Mg) ** 2)) / (2 * (n_bp - 1))
+
+            return 1 / tem - kelvins
+
+    # return (H * 1000) / (S + 1.987 * math.log10((c_t ** -3) / 4)) - 273.15 + 16.6 * math.log10(1.)
+
+    # return (H * 1000) / (S + 1.987 * math.log10((c_t ** -3) / 4)) - 273.15 + 16.6 * math.log10(1.)
